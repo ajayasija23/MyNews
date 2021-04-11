67691ead5b18b8444e19179073a2618ca895e2ef
Asija <ajayasija23@gmail.com>
2021-04-11 12:59:00 +0530

    Second Commit

--- app/src/main/java/com/example/mynews/activities/MainActivity.java
+++ app/src/main/java/com/example/mynews/activities/MainActivity.java
@@ -9,9 +9,11 @@ import android.widget.AdapterView;
 import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.paging.PagedList;
 
 import com.example.mynews.R;
 import com.example.mynews.adapters.NewsAdapter;
+import com.example.mynews.adapters.NewsFeedAdapter;
 import com.example.mynews.databinding.ActivityMainBinding;
 import com.example.mynews.fragment.CountryBottomSheetFragment;
 import com.example.mynews.fragment.SourceBottomSheetFragment;
@@ -19,6 +21,7 @@ import com.example.mynews.listener.BottomSheetListener;
 import com.example.mynews.model.NewsModel;
 import com.example.mynews.util.Constants;
 import com.example.mynews.util.FrequentFuctions;
+import com.example.mynews.viewmodel.NewsFeedViewModel;
 import com.example.mynews.viewmodel.NewsViewModel;
 import com.google.android.material.button.MaterialButton;
 
@@ -27,12 +30,13 @@ import java.util.LinkedHashMap;
 public class MainActivity extends BaseActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener, BottomSheetListener, SearchView.OnQueryTextListener {
 
     private ActivityMainBinding binding;
-    private NewsViewModel viewModel;
-    private LinkedHashMap map;
+    private NewsFeedViewModel viewModel;
+    private LinkedHashMap map=new LinkedHashMap();
     private BottomSheetListener listener;
     private CountryBottomSheetFragment countryBottomSheetFragment;
     private SourceBottomSheetFragment bottomsheet;
     private MaterialButton button;
+    private NewsFeedAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +51,7 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemSele
             View view=binding.getRoot();
             setContentView(view);
             listener=this;
-            fetchNews();
+            loadInitialNews();
             setListener();
         }
         else {
@@ -58,6 +62,18 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemSele
         }
     }
 
+    private void loadInitialNews() {
+        adapter=new NewsFeedAdapter(MainActivity.this);
+        viewModel=ViewModelProviders.of(this).get(NewsFeedViewModel.class);
+        viewModel.getArticlesPagedList().observe(this, new Observer<PagedList<NewsModel.ArticlesBean>>() {
+            @Override
+            public void onChanged(PagedList<NewsModel.ArticlesBean> articlesBeans) {
+                adapter.submitList(articlesBeans);
+            }
+        });
+        binding.rvNews.setAdapter(adapter);
+    }
+
     private void setListener() {
         binding.spinnerSort.setOnItemSelectedListener(this);
         binding.btnFilter.setOnClickListener(this);
@@ -66,43 +82,11 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemSele
         binding.ivSearch.setOnClickListener(this);
     }
 
-    private void fetchNews() {
-        showProgress();
-        viewModel= ViewModelProviders.of(this).get(NewsViewModel.class);
-        map=new LinkedHashMap();
-        map.put("apiKey", Constants.API_KEY);
-        map.put("country","in");
-        viewModel.fetchNews(map);
-        viewModel.getNews().observe(this, new Observer<NewsModel>() {
-            @Override
-            public void onChanged(NewsModel newsModel) {
-                hideProgress();
-                binding.searchView.clearFocus();
-                if (newsModel!=null){
-                    if (newsModel.getTotalResults()!=0){
-                        binding.rlSort.setVisibility(View.VISIBLE);
-                        binding.rvNews.setVisibility(View.VISIBLE);
-                        binding.llNoresult.setVisibility(View.GONE);
-                        binding.rvNews
-                                .setAdapter(
-                                        new NewsAdapter(newsModel.getArticles(),MainActivity.this)
-                                );
-                    }
-                    else {
-                        binding.rlSort.setVisibility(View.GONE);
-                        binding.rvNews.setVisibility(View.GONE);
-                        binding.llNoresult.setVisibility(View.VISIBLE);
-                    }
-                }
-            }
-        });
-    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        map.put("sortBy",parent.getSelectedItem().toString().toLowerCase());
-        showProgress();
-        viewModel.fetchNews(map);
+
     }
 
     @Override
@@ -142,22 +126,16 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemSele
     @Override
     public void onSelectSources(String sources) {
         //apply source filter
-        bottomsheet.dismiss();
-        if (map.containsKey("country"))
-            map.remove("country");
-        map.put("sources",sources);
-        showProgress();
-        viewModel.fetchNews(map);
+
     }
 
     @Override
     public void onSelectCountry(String country) {
-        //apply country filter
         countryBottomSheetFragment.dismiss();
-        binding.tvLocation.setText(country);
-        map.put("country", FrequentFuctions.getCountryCodes(country));
+        map.put("apiKey",Constants.API_KEY);
+        map.put("country",FrequentFuctions.getCountryCodes(country));
         showProgress();
-        viewModel.fetchNews(map);
+        viewModel.changeMap(map);
     }
 
     public BottomSheetListener getListener() {
@@ -166,26 +144,12 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemSele
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        map.put("q",query);
-        if (map.containsKey("country"))
-            map.remove("country");
-        showProgress();
-        viewModel.fetchNews(map);
         return false;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        if (newText.isEmpty()){
-            map.put(
-                    "country",
-                    FrequentFuctions.getCountryCodes(binding.tvLocation.getText().toString())
-            );
-            if (map.containsKey("q"))
-                map.remove("q");
-            showProgress();
-            viewModel.fetchNews(map);
-        }
+
         return false;
     }
 
--- app/src/main/java/com/example/mynews/viewmodel/NewsFeedViewModel.java
+++ app/src/main/java/com/example/mynews/viewmodel/NewsFeedViewModel.java
@@ -8,16 +8,31 @@ import androidx.paging.PagedList;
 
 import com.example.mynews.model.NewsModel;
 import com.example.mynews.repository.NewsItemDataSourceFactory;
+import com.example.mynews.util.Constants;
 
 import java.util.LinkedHashMap;
 
 public class NewsFeedViewModel extends ViewModel {
+    private LinkedHashMap<String,String> initialMap;
     private  NewsItemDataSourceFactory factory;
     private LiveData<PagedList<NewsModel.ArticlesBean>> articlesPagedList;
     private LiveData<PageKeyedDataSource<Integer, NewsModel.ArticlesBean>> liveArticles;
 
-    NewsFeedViewModel(){
-        factory =new NewsItemDataSourceFactory(null);
+    public NewsFeedViewModel(){
+        initialMap=new LinkedHashMap<>();
+        initialMap.put("apiKey", Constants.API_KEY);
+        initialMap.put("country", "in");
+        factory =new NewsItemDataSourceFactory(initialMap);
+        liveArticles=factory.getItemLiveDataSource();
+        PagedList.Config config =
+                (new PagedList.Config.Builder())
+                        .setEnablePlaceholders(false)
+                        .setPageSize(20)
+                        .build();
+        articlesPagedList=(new LivePagedListBuilder(factory,config)).build();
+    }
+    public void changeMap(LinkedHashMap<String,String> map){
+        factory =new NewsItemDataSourceFactory(map);
         liveArticles=factory.getItemLiveDataSource();
         PagedList.Config config =
                 (new PagedList.Config.Builder())
